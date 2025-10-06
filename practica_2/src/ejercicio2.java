87006691f78c7c62807f0e437b87e581c1e247e0
Mario <mescrico@gmail.com>
2025-10-06 11:45:25 +0200

    ejercicio2 hecho

---
+++ practica_2/src/ejercicio2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ejercicio2 {
+    //Muestra por pantalla todos los elementos de un array
+    //de números enteros separados por un espacio
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] x = {1,2,3,4,5};
+
+        for(int i=0; i<5; i++) {
+            System.out.print(x[i]+" ");
+        }
+
+
+
+
+    }
+}
