555e29072cf7c8e157eb40a094c436e15f7e1471
Mario <mescrico@gmail.com>
2025-10-29 19:40:08 +0100

    ejercicio5 sin completar

--- practica3/README.md
+++ practica3/README.md
@@ -5,3 +5,6 @@
 
 - ### [Ejercicio 1](src/ejercicio1.java)
 - ### [Ejercicio 2](src/ejercicio2.java)
+- ### [Ejercicio 3](src/ejercicio3.java)
+- ### [Ejercicio 4](src/ejercicio4.java)
+- ### [Ejercicio 5](src/ejercicio5.java)
--- practica3/src/ejercicio5.java
+++ practica3/src/ejercicio5.java
@@ -11,35 +11,56 @@ public class ejercicio5 {
         int maximo = 0;
         int minimo = 0;
         int maximoC = 0;
+        String posmax = "";
+        String posmin = "";
+        int sumaT;
+        int sumaF = 0;
         //Pongo que minimoC valga 1001 para hacer las posteriores comparaciones q
         int minimoC = 1001;
 
         for (int i = 0; i < numeros.length; i++) {
             for (int j = 0; j < numeros[i].length; j++) {
                 numeros[i][j] = (int) (Math.random()*1001);
+                System.out.printf("|%-10d|", numeros[i][j]);
             }
+            System.out.printf("%n");
         }
+
         for (int i = 0; i < numeros.length; i++) {
+            sumaT = 0;
             if(i == 0) {
                 maximo = numeros[i][0];
                 minimo = numeros[i][0];
             }
             for (int j = 0; j < numeros[i].length; j++) {
+
                 if(maximo < numeros[i][j]) {
                     maximo = numeros[i][j];
+                    posmax = "Fila "+(i+1)+" y Columna "+(j+1);
                 }
                 if(minimo > numeros[i][j]) {
                     minimo = numeros[i][j];
+                    posmin = "Fila "+(i+1)+" y Columna "+(j+1);
                 }
+                sumaT += numeros[i][j];
             }
+
             if(maximo > maximoC) {
                 maximoC = maximo;
+            }
+            if(i == numeros.length-1) {
+                System.out.println("La posición del numero maximo ("+maximoC+") es "+posmax);
             }
             if(minimo < minimoC) {
                 minimoC = minimo;
             }
+            if(i == numeros.length-1) {
+                System.out.println("La posición del numero minimo ("+minimoC+") es "+posmin);
+            }
+            sumaF += sumaT;
+            if(i == numeros.length-1) {
+                System.out.println("Suma de las filas: "+sumaF);
+            }
         }
-        System.out.println(maximoC);
-        System.out.println(minimoC);
     }
 }
