4a3c74bb26716ce453f967fbe5a5960c92f16123
Mario <mescrico@gmail.com>
2025-10-30 14:01:23 +0100

    ejercicio5 completado

--- practica3/src/ejercicio5.java
+++ practica3/src/ejercicio5.java
@@ -7,7 +7,9 @@ public class ejercicio5 {
     //- la suma de todas las columnas
     //- la suma de todas las filas.
     public static void main(String[] args) {
+        //Creamos el array bidimensional de 6 filas y 10 columnas
         int[][] numeros = new int[6][10];
+        //Creamos todas las variables que se utilizarán
         int maximo = 0;
         int minimo = 0;
         int maximoC = 0;
@@ -19,58 +21,70 @@ public class ejercicio5 {
         //Pongo que minimoC valga 1001 para hacer las posteriores comparaciones q
         int minimoC = 1001;
 
-
+        //Creamos el bucle para rellenar el array con numeros aleatorios entre 0 y 1000
         for (int i = 0; i < numeros.length; i++) {
             for (int j = 0; j < numeros[i].length; j++) {
                 numeros[i][j] = (int) (Math.random()*1001);
-                System.out.printf("|%-10d|", numeros[i][j]);
             }
-            System.out.printf("%n");
         }
-
+        //Creamos el bucle para buscar la posición de los valores minimo y maximo
         for (int i = 0; i < numeros.length; i++) {
+            //Guardamos el primer valor para que no se reinicie en cada iteracion
             if(i == 0) {
                 maximo = numeros[i][0];
                 minimo = numeros[i][0];
             }
+            //Creamos el bucle que busque la posición
             for (int j = 0; j < numeros[i].length; j++) {
-
+                //Si maximo es menor que el numero actual
                 if(maximo < numeros[i][j]) {
+                    //Maximo sera ese numero
                     maximo = numeros[i][j];
                     posmax = "Fila "+(i+1)+" y Columna "+(j+1);
                 }
+                //Si minimo es mayor que el numero actual
                 if(minimo > numeros[i][j]) {
+                    //Minimo sera ese numero
                     minimo = numeros[i][j];
                     posmin = "Fila "+(i+1)+" y Columna "+(j+1);
                 }
             }
-
+            //Si el maximo actual es mayor que el maximo global (que en la primera iteracion es 0)
             if(maximo > maximoC) {
+                //Maximo global sera ese maximo
                 maximoC = maximo;
             }
-
+            //Si el minimo actual es menor que el minimo global (que en la primera iteracion es 1001)
             if(minimo < minimoC) {
+                //Minimo global sera ese minimo
                 minimoC = minimo;
             }
         }
         System.out.println("La posición del numero maximo ("+maximoC+") es "+posmax);
         System.out.println("La posición del numero minimo ("+minimoC+") es "+posmin);
 
+        //Bucle para calcular las sumas de las filas
         for (int i = 0; i < numeros.length; i++) {
             sumaT = 0;
+            //Bucle para la suma de cada fila
             for (int j = 0; j < numeros[i].length; j++) {
                 sumaT += numeros[i][j];
-                for (int k = 0; k < numeros.length; k++) {
-                    sumaC += numeros[k][j];
-
-                }
             }
-            System.out.println("Fila "+(i+1)+": "+sumaT);
-            System.out.println("Columna "+(i+1)+": "+sumaC);
+            System.out.println("Suma de la fila "+(i+1)+": "+sumaT);
             sumaF += sumaT;
+        }
+        //Bucle para calcular las sumas de las columnas
+        for (int j = 0; j < numeros[0].length; j++) {
+            sumaT = 0;
+            //Bucle para la suma de las columnas
+            for (int i = 0; i < numeros.length; i++) {;
+                sumaT += numeros[i][j];
+            }
+            System.out.println("Suma de la columna "+(j+1)+": "+sumaT);
+            sumaC += sumaT;
         }
         System.out.println("Suma de las filas: "+sumaF);
         System.out.println("Suma de las columnas: "+sumaC);
-        System.out.println("Suma total de todas las filas y columnas: ");
+        System.out.println("Suma total de todas las filas y columnas: "+sumaF);
     }
 }
