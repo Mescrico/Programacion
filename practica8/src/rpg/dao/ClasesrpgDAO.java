dc5ad3d6bd4dc58e8dafd4f71d1d7f4aef7292d2
mario <mescrico@gmail.com>
2026-04-06 14:24:13 +0200

    Creacion de ClasesrpgDAO

--- practica8/src/rpg/dao/ClasesrpgDAO.java
+++ practica8/src/rpg/dao/ClasesrpgDAO.java
@@ -1,6 +1,7 @@
 package rpg.dao;
 
 import rpg.model.Clases_RPG;
+import rpg.model.Habilidades;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -26,16 +27,34 @@ public class ClasesrpgDAO {
     public void cargarClases() {
         try {
           Statement st1 = connection.createStatement();
+          Statement st2 = connection.createStatement();
           ResultSet rs1 = st1.executeQuery("SELECT * FROM CLASES_RPG");
+          ArrayList<Habilidades> listahabilidades = new ArrayList<>();
 
           while(rs1.next()) {
             int id = rs1.getInt("id");
             String nombre = rs1.getString("nombre");
 
-            Clases_RPG clasesRPG = new Clases_RPG(id, nombre);
-            clases.add(clasesRPG);
+            ResultSet rs2 = st2.executeQuery("SELECT * FROM HABILIDADES WHERE id_clase = "+id);
+
+            while (rs2.next()) {
+                int id_habilidad = rs2.getInt("id");
+                String nombre_habilidad = rs2.getString("nombre");
+                int dano_base = rs2.getInt("dano_base");
+                int usos_maximos = rs2.getInt("usos_maximos");
+                int id_clase = rs2.getInt("id_clase");
+
+                Habilidades habilidad = new Habilidades(id_habilidad, nombre_habilidad, dano_base, usos_maximos, id_clase);
+                listahabilidades.add(habilidad);
+
+            }
+
+            Clases_RPG claseRPG = new Clases_RPG(id, nombre);
+            claseRPG.setListahabilidades(listahabilidades);
+
+            clases.add(claseRPG);
           }
-        } catch (SQLException e ) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
