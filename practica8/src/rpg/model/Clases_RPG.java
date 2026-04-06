4da1c0f4bf86b905626022c60035627379d4f559
mario <mescrico@gmail.com>
2026-04-06 14:23:03 +0200

    Modificacion Clases_RPG

--- practica8/src/rpg/model/Clases_RPG.java
+++ practica8/src/rpg/model/Clases_RPG.java
@@ -1,12 +1,16 @@
 package rpg.model;
 
+import java.util.ArrayList;
+
 public class Clases_RPG {
     private int idClasesRPG;
     private String nombre;
+    private ArrayList<Habilidades> listahabilidades;
 
     public Clases_RPG(int idClasesRPG, String nombre) {
         this.idClasesRPG = idClasesRPG;
         this.nombre = nombre;
+        this.listahabilidades = new ArrayList<>();
     }
 
     public int getIdClasesRPG() {
@@ -23,6 +27,18 @@ public class Clases_RPG {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public void addListahabilidades(Habilidades h) {
+        listahabilidades.add(h);
+    }
+
+    public ArrayList<Habilidades> getListahabilidades() {
+        return listahabilidades;
+    }
+
+    public void setListahabilidades(ArrayList<Habilidades> listahabilidades) {
+        this.listahabilidades = listahabilidades;
     }
 
     @Override
